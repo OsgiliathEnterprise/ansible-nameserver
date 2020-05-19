b814ff9f5616da98db3d94f3c86cbab01dd274c8
Charlie Mordant <cmordant1@gmail.com>
2020-05-19 23:16:45 +0200

    hostname resolution

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,32 @@
+"""Role testing files using testinfra."""
+
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    f = host.file("/etc/hosts")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+
+def test_hosts_file_contains_the_new_entry(host):
+    command = """cat /etc/hosts | \
+    egrep -c '127\.0\.0\.1\sdummy.dum.com\s'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_hosts_file_contains_the_new_ip6_entry(host):
+    command = """cat /etc/hosts | \
+    egrep -c '::1\sdummy.dum.com\s'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_hostname_is_updated(host):
+    """Validate /etc/hosts file."""
+    command = """hostname | \
+    egrep -c '^dummy.dum.com$'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
