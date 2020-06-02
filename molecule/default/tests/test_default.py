145e58bb70f8cdec711b263dbb02bce0cc79102a
Charlie Mordant <cmordant1@gmail.com>
2020-06-03 01:17:39 +0200

    bind9 installation on docker

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,20 +1,11 @@
 """Role testing files using testinfra."""
 
 
-def test_hosts_file(host):
-    """Validate /etc/hosts file."""
-    f = host.file("/etc/hosts")
-
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
-
-
 def test_hosts_file_contains_the_new_entry(host):
     command = r"""cat /etc/hosts | \
     egrep -c '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\sdummy.dum.com'"""
     cmd = host.run(command)
-    assert '1' in cmd.stdout
+    assert '2' in cmd.stdout
 
 
 def test_hosts_file_contains_the_new_ip6_entry(host):
@@ -22,12 +13,45 @@ def test_hosts_file_contains_the_new_ip6_entry(host):
     egrep -c \
     '\w{0,4}:\w{0,4}:\w{0,4}:\w{0,4}:\w{0,4}:\w{0,4}\sdummy.dum.com'"""
     cmd = host.run(command)
-    assert '1' in cmd.stdout
+    assert '2' in cmd.stdout
 
 
 def test_hostname_is_updated(host):
-    """Validate /etc/hosts file."""
     command = r"""hostname | \
     egrep -c '^dummy.dum.com$'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
+
+
+def test_lookup_resolves_google(host):
+    command = r"""nslookup google.fr 127.0.0.1 | \
+    grep -Pzoc 'Non-authoritative answer:\nName:\s+google.fr'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_lookup_resolves_node(host):
+    command = r"""nslookup node0.osgiliath.net 127.0.0.1 | \
+    grep -c '192.168.1.1'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_lookup_resolves_reverse(host):
+    command = r"""nslookup 192.169.0.2 127.0.0.1 | \
+    grep -c 'node0.osgiliath.net.169.192.in-addr.arpa'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_resolv_conf_updated(host):
+    command = r"""cat /etc/resolv.conf | grep -c nameserver"""
+    cmd = host.run(command)
+    assert '3' in cmd.stdout
+
+def test_lookup_resolves_node_as_client(host):
+    command = r"""nslookup node0.osgiliath.net | \
+    grep -c '192.168.1.1'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
