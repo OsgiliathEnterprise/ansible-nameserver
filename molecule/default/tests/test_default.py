e80fbbed791590d34b66a3ba407cfc2064743bb6
Charlie Mordant <cmordant1@gmail.com>
2021-05-27 21:49:49 +0200

    fix molecule tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,36 +4,28 @@
 def test_hosts_file_contains_the_new_entry(host):
     command = r"""cat /etc/hosts | \
     egrep -c \
-    '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\sdns\.osgiliath\.test'"""
-    cmd = host.run(command)
-    assert int(cmd.stdout) >= 1
-
-
-def test_hosts_file_contains_the_new_ip6_entry(host):
-    command = r"""cat /etc/hosts | \
-    egrep -c \
-    '\w{0,4}:\w{0,4}:\w{0,4}:\w{0,4}:\w{0,4}:\w{0,4}\sdns\.osgiliath\.test'"""
+    '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s+dns\.osgiliath\.test'"""
     cmd = host.run(command)
     assert int(cmd.stdout) >= 1
 
 
 def test_additional_hostname1_added(host):
     command = r"""cat /etc/hosts | \
-    egrep -c '^192\.168\.122\.1\s*idm\.osgiliath\.test$'"""
+    egrep -c '^192\.168\.122\.1\s+idm\.osgiliath\.test$'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
 
 def test_additional_hostname2_added(host):
     command = r"""cat /etc/hosts | \
-    egrep -c '^192\.168\.122\.2\s*infra\.osgiliath\.test$'"""
+    egrep -c '^192\.168\.122\.2.+infra\.osgiliath\.test'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
 
 def test_hostname_is_updated(host):
     command = r"""hostname | \
-    egrep -c '^dummy.dum.com$'"""
+    grep -c 'dns.osgiliath.test'"""
     cmd = host.run(command)
     assert '1' in cmd.stdout
 
