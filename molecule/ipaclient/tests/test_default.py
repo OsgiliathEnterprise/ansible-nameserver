eb0278e60259891e0548828a28d6fc2c547b44e6
Charlie Mordant <cmordant1@gmail.com>
2021-06-13 19:04:54 +0200

    [travis skip] standard PTR zone name

--- molecule/ipaclient/tests/test_default.py
+++ molecule/ipaclient/tests/test_default.py
@@ -6,7 +6,7 @@ def test_ipa_zone_is_created(host):
     command = r"""set -o pipefail && echo '123ADMin'| \
     kinit admin > /dev/null && \
     ipa dnszone-find --pkey-only | \
-    egrep -c '0/16\.0\.[0-9]{1,3}\.[0-9]{1,3}\.in-addr\.arpa\.'"""
+    egrep -c '[0-9]{1,3}\.[0-9]{1,3}\.in-addr\.arpa\.'"""
     cmd = host.run(command)
     assert int(cmd.stdout) >= 1
 
